0229a8954b66635d70ae4e1d67456df05bac5650
Kane Milligan <kane.milligan@glean.co>
2023-11-26 18:47:57 +0000

    fixed active menu item

--- app/(home)/_components/sidebar-item.jsx
+++ app/(home)/_components/sidebar-item.jsx
@@ -5,12 +5,13 @@ import { MdOutlineDashboard } from "react-icons/md";
 import { PiPlant } from "react-icons/pi";
 import { FaSearch } from "react-icons/fa";
 import { IoPeopleOutline } from "react-icons/io5";
+import { cn } from "@/lib/utils";
 
 //TODO fix icons
 const SideBarItem = ({ icon, label, href, id }) => {
   const pathname = usePathname();
   const router = useRouter();
-  //TODO handle active sidebar
+
   const isActive = (pathname === "/" && href === "/") || pathname === href;
 
   const onClick = () => {
@@ -21,14 +22,18 @@ const SideBarItem = ({ icon, label, href, id }) => {
     <button
       onClick={onClick}
       type="button"
-      className="flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-lime-100/60"
+      className={cn(
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-lime-100/60",
+        isActive &&
+          "text-lime-600 bg-lime-200/60 hover:bg-lime-200/20 hover:text-lime-600"
+      )}
     >
       <div className="flex items-center gap-x-2 py-4">
         <icon size={22} className="text-slate-500" />
-        {id == 1 && <MdOutlineDashboard size={22}/>}
-        {id == 2 && <PiPlant size={22}/>}
-        {id == 3 && <FaSearch size={22}/>}
-        {id == 4 && <IoPeopleOutline size={22}/>}
+        {id == 1 && <MdOutlineDashboard size={22} />}
+        {id == 2 && <PiPlant size={22} />}
+        {id == 3 && <FaSearch size={22} />}
+        {id == 4 && <IoPeopleOutline size={22} />}
         {label}
       </div>
     </button>
