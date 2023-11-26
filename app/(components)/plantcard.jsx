b206643d8fbf13177ea26d5947ac9071fb63aec3
Kane Milligan <kane.milligan@glean.co>
2023-11-26 10:56:35 +0000

    fix image sizing

--- app/(components)/plantcard.jsx
+++ app/(components)/plantcard.jsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 import React from "react";
 
-const PlantCard = ({id, name, watering, imgUrl }) => {
+const PlantCard = ({ id, name, watering, imgUrl }) => {
   return (
     <Link
       href={{
-        pathname: '/plant',
+        pathname: "/plant",
         query: { id: id },
-      }}>
+      }}
+    >
       <div className="flex flex-col items-center justify-evenly p-2 bg-white w-60 h-60 rounded shadow-sm hover:bg-neutral-50 cursor-pointer">
         <Image
           src={imgUrl}
           alt={`Image of ${name}`}
           width={200}
           height={200}
-          className="rounded h-[200px] w-[200px]"
+          className="rounded h-[200px] w-[200px] overflow-hidden"
         />
         <p>{name}</p>
         <p>{watering}</p>
